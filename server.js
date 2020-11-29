d002f9678b9a664ab117f4a3080b55a694c9dfe8
Luis <luisedumlg@gmail.com>
2020-11-28 21:44:39 -0300

    Adicionando o forms com get e o upload do arquivo com post

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const app = express();
+
+app.use(express.static('public'))
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,'uploads/')
+    },
+    filename: function(req,file,cb){
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+app.get('/index.html', (req,res)=>{
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.get('/process_get', (req,res)=>{
+    const response = {
+        firstName:req.query.first_name,
+        lastName: req.query.last_name
+    }
+    console.log(response);
+    res.end(JSON.stringify(response))
+})
+
+app.post('/file_upload', 
+    upload.single('file'),
+    (req,res) => res.send('<h2>Upload realizado com sucesso</h2>')
+);
+
+app.listen('8081', () => console.log('App na porta 8081'));
